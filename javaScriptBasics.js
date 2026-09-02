41cd353733c2a481c83afd31f982e262ad0000d1
palaniselvam <j.palaniselvam@gmail.com>
2026-09-02 10:52:58 +0530

    Add files via upload
    Day 2 - Assignments

---
+++ javaScriptBasics.js
@@ -0,0 +1,37 @@
+console.log("welcome to Playwright")
+/*
+a) firstName    
+ b) companyName     
+ c) mobileNumber     
+ d) isAutomation     
+ e) hasPlaywright (do not assign)  
+ Print and confirm the values and data types */
+
+ var firstName ="Palani"
+ console.log(firstName);
+ console.log(typeof firstName);
+
+ var companyName ="xyz"
+ console.log(companyName);
+ console.log(typeof companyName);
+
+ var mobileNumber =1234567890
+ console.log(mobileNumber);
+ console.log(typeof mobileNumber);
+ 
+ var isAutomation = true
+ console.log(isAutomation);
+ console.log(typeof isAutomation);
+
+ var hasPlaywright
+ console.log(hasPlaywright);
+ console.log(typeof hasPlaywright);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
